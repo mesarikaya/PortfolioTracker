66b6a2835ec6b9a8f1920c1331a0078c1548a2e5
Ergin Sarikaya <mesarikaya@users.noreply.github.com>
2018-05-04 11:50:15 +0200

    Update autocomplete.js

--- Controllers/autocomplete.js
+++ Controllers/autocomplete.js
@@ -7,7 +7,7 @@ function autocomplete(db) {
 
     this.search = function(req, res){
         //console.log("searching for data:", req.body.stock_name_data);
-        Stocks.find({ $or:[{'Ticker': { $regex: new RegExp(".*"+req.body.stock_name_data+".*", "i")}}, {'Name': { $regex: new RegExp(".*"+req.body.stock_name_data+".*", "i")}}]}).sort({'Weight': 1}).limit(5)
+        Stock.find({ $or:[{'Ticker': { $regex: new RegExp(".*"+req.body.stock_name_data+".*", "i")}}, {'Name': { $regex: new RegExp(".*"+req.body.stock_name_data+".*", "i")}}]}).sort({'Weight': 1}).limit(5)
         .exec(function (err, doc) { 
             if (err) { console.log("Stock list database search resulted with error:", err);}
             if (doc){
