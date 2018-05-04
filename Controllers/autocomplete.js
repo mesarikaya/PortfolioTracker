7a801f05051e30fc1c8503188565ae0edc2042b3
Ergin Sarikaya <mesarikaya@users.noreply.github.com>
2018-05-04 12:26:08 +0200

    Update autocomplete.js

--- Controllers/autocomplete.js
+++ Controllers/autocomplete.js
@@ -1,23 +1,30 @@
 'use strict';
+try {
+    var Stock = require('../Model/stock_list.js');
+    console.log("Inside the autocomplete");        
+    console.log("Stock list:", Stock);
+    function autocomplete(db) {
 
-var Stocks = require('../Model/stock_list.js');
-console.log("Inside the autocomplete");        
-console.log("Stock list:", Stocks);
-function autocomplete(db) {
+        this.search = function(req, res){
+            console.log("searching for data:", req.body.stock_name_data);
+            Stock.find({ $or:[{'Ticker': { $regex: new RegExp(".*"+req.body.stock_name_data+".*", "i")}}, {'Name': { $regex: new RegExp(".*"+req.body.stock_name_data+".*", "i")}}]}).sort({'Weight': 1}).limit(5)
+            .exec(function (err, doc) { 
+                if (err) { console.log("Stock list database search resulted with error:", err);}
+                if (doc){
+                    //console.log("Stock list:", doc);
+                    res.json(doc);
+                }
+            });
+        };
 
-    this.search = function(req, res){
-        //console.log("searching for data:", req.body.stock_name_data);
-        Stocks.find({ $or:[{'Ticker': { $regex: new RegExp(".*"+req.body.stock_name_data+".*", "i")}}, {'Name': { $regex: new RegExp(".*"+req.body.stock_name_data+".*", "i")}}]}).sort({'Weight': 1}).limit(5)
-        .exec(function (err, doc) { 
-            if (err) { console.log("Stock list database search resulted with error:", err);}
-            if (doc){
-                //console.log("Stock list:", doc);
-                res.json(doc);
-            }
-        });
-    };
-    
+    }
+
 }
-    
+catch(error) {
+  console.error(error);
+  // expected output: SyntaxError: unterminated string literal
+  // Note - error messages will vary depending on browser
+}
+
 
 module.exports = autocomplete;
